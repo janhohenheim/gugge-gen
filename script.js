e89e2b6b5e11b1625d974bb5c6888c47b3e5a028
Jan Nils Ferner <jn_ferner@hotmail.de>
2018-12-19 16:18:26 +0100

    Make rainbow button dynamic

--- script.js
+++ script.js
@@ -34,27 +34,27 @@ document.getElementById('generate').addEventListener('click', () => {
     const generatedName = isNumericalIdentifier ? `${identifier} ${noun}` : `${identifier}${noun.toLowerCase()}`
     document.getElementById('generated-name').textContent = generatedName
 
+    document.getElementById('rainbow').classList.remove(["hidden"])
+
+
     console.log(`${identifier} ${noun}`)
 });
 
-document.getElementById("js-magic").addEventListener("click", () => {
 
-    document.getElementById('generated-name').style.color = getRandomColor()
-    
-    
-
-
-
+document.getElementById("rainbow-btn").addEventListener("click", () => {
+    window.setInterval(() => {
+        document.getElementById('generated-name').style.color = getRandomColor()
+    }, 250);
 });
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
+        color += letters[Math.floor(Math.random() * 16)];
     }
     return color;
-  }
+}
 
 function generateNumericalIdentifier() {
     const randomNumber = Math.floor(Math.random() * 100)
